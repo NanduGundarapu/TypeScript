9a14c69fd7635814a6ec7f2362b12fea65c50d42
Nanda Kishore <nandugundarapu@gmail.com>
2020-09-15 11:34:47 +0530

    Advanced types

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,174 @@
-//type addFn = (a : number , b : number) => number;
+type Admin = {
+  name: string;
+  privileges: string[];
+};
 
-interface AddFn{        // interface with function type , similar to function type shown above
-    (a : number , b : number ) : number;
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee; // intersection type
+
+const el: ElevatedEmployee = {
+  name: "Nandu",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+console.log(el);
+
+//can also be implemented using interfaces as shown below
+// interface IAdmin{
+//     name : string;
+//     privileges : string[];
+// }
+
+// interface IEmployee{
+//     name : string;
+//     startDate : Date;
+// }
+
+// interface IElevatedEmployee extends IEmployee,IAdmin{}
+
+// const iel : IElevatedEmployee = {
+//     name : 'Nandu',
+//     privileges : ['create-server'],
+//     startDate : new Date()
+// }
+
+// console.log(iel);
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric; // Universal will be of type number
+
+//Function overloads to give typescript knowlege about return type expected
+
+function add(a:number,b:number) : number;
+function add(a:string,b:string) : string;
+function add(a:number,b:string) : string;
+function add(a:string,b:number) : string;
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string")
+    // this is called type guard using typeof
+    return a.toString() + b.toString();
+  else return a + b;
 }
 
-interface Named{
-    readonly name? : string;    // properties cant be initialized like name : string = "Nandu" 
-    outputName? : string;
+const result = add('Nanda ','Kishore'); // with overloads type script infers result correctly as string
+result.split(" ")
+
+const fetchedUserData = {
+  id : 'u1',
+  name : 'Nanda',
+  //job : { title : 'CEO' , description : 'My imaginary company'}
 }
 
-interface Greetable extends Named { 
-    greet(phrase? : string) : void;
+//console.log(fetchedUserData?.job?.title); // optional chaining
+
+const userInput = undefined;
+
+const storageData = userInput ?? 'DEFAULT'; // userInput || 'DEFAULT' returns 'DEFAULT' if userInput is undefined or null or '' or 0
+console.log(storageData);                   // Nullish coalescing operator ?? returns 'DEFAULT' only if userinput is null or undefined
+
+type UnknownEmployee = Admin | Employee;
+
+// another use case for type guard is when union type is to be checked
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name " + emp.name);
+
+  if ("privileges" in emp) {
+    console.log("Previleges " + emp.privileges);
+  }
+
+  if ("startDate" in emp) {
+    console.log("Start Date " + emp.startDate);
+  }
 }
 
-let user1 : Greetable;
-let user2 : Greetable;
+printEmployeeInformation(el);
 
-class Person implements Greetable{
-    name? : string;
-    age = 26;
-    constructor(n? : string){
-        if(n){
-            this.name = n;
-        }
+printEmployeeInformation({ name: "Nandu", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving a car...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading cargo..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck)
+    //more elegant way instead of doing 'loadCargo' in vehicle
+    vehicle.loadCargo(1000); // when working with union types and classes
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+
+//Descriminated unions
+interface Bird {
+  type : 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type : 'horse'
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+    let speed;
+    switch (animal.type) {
+      case 'bird':
+        speed = animal.flyingSpeed;
+        break;
+      case 'horse':
+        speed = animal.runningSpeed;
     }
-    greet(phrase? : string){
-        if(this.name){
-            console.log(phrase+" "+this.name);
-        }
-        else{
-            console.log('Hi!!!');
-        }
-    }
+    console.log('Moving with speed : '+ speed);
 }
-user1 = new Person("Nandu");
-user2 = new Person(); // uses optional properties
 
-//user1.name = "Nanda" // gives compile error as name is readonly in the interface
+moveAnimal({type:'bird',flyingSpeed : 10});
 
-user1.greet("Hi there, Iam");
+moveAnimal({type:'horse',runningSpeed: 10});
 
-console.log(user1);
+//const userInputElement = <HTMLInputElement>document.getElementById("user-input")!;   // works with typescript , but causes conflicts with react js syntax
 
-user2.greet();
+const userInputElement = document.getElementById('user-input')! as HTMLInputElement ; // works for React JSX syntax too 
+userInputElement.value = 'Hi buddy!';
 
-console.log(user2);
+//alternative to '!'
+// const userInputElement = document.getElementById('user-input');
+// if(userInputElement){
+//   (userInputElement as HTMLInputElement).value = 'Hi there!';
+// }
+
+interface ErrorContainer{
+  [prop:string]: string;
+}
+
+const errorBag : ErrorContainer = {
+  email : 'Not a valid email!!',
+  username : 'Must start with a capital letter'
+}
+console.log(errorBag);
