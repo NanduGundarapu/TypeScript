2990b242d53940c42c30c96c64a826ccc9c97cdd
Nanda Kishore <nandugundarapu@gmail.com>
2020-09-18 18:22:53 +0530

    Decorators

--- src/app.ts
+++ src/app.ts
@@ -1,102 +1,201 @@
-// const names : Array<string> = [];  // string[]
-// names[0].split(' ')
-
-// const promise : Promise<string> = new Promise((resolve,reject) => {
-//   setTimeout(()=>{
-//     resolve('This is finally done !!');
-//   },2000);
-// });
-
-// promise.then(data => data.split(' '));
-
-//Generic Types gives flexibility with type support
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
+function Logger(logString: string) {
+  console.log("LOGGER FACTORY");
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-const mergedObj = merge({ name: "Nandu", hobbies: ["sports"] }, { age: 26 });
-console.log(mergedObj);
-
-interface Lengthy {
-  length: number;
+function WithTemplate(template: string, hookId: string) {
+  console.log("TEMPLATE FACTORY");
+  return function <
+    T extends {
+      new (...args: any[]): {
+        firstName: string;
+        lastName: string;
+        initial: string;
+        role: string;
+      };
+    }
+  >(originalConstructor: T) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super(..._);
+        console.log("Rendering template");
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector(
+            "h1"
+          )!.textContent += `${this.firstName} ${this.lastName} ${this.initial}`;
+          hookEl.querySelector("h2")!.textContent += this.role;
+        }
+      }
+    };
+  };
 }
 
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-  let descriptionText = "Got no values";
-  if (element.length == 1) {
-    descriptionText = "Got 1 element";
-  } else if (element.length > 1) {
-    descriptionText = "Got " + element.length + " elements";
+@Logger("Logging In....")
+@WithTemplate("<h1>Welcome - </h1><h2>Role - <h2>", "app")
+class Person {
+  role = "Developer";
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public initial: string
+  ) {
+    console.log("Creating Person object....");
   }
-  return [element, descriptionText];
 }
 
-console.log(countAndDescribe(["Nanda", "Kishore"]));
-console.log(countAndDescribe("Hi there!"));
-console.log(countAndDescribe([]));
+const pers = new Person("Nanda", "Kishore", "G");
+// console.log(pers);
 
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
+function Log(target: any, propertyName: string | symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+
+function Log2(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
 ) {
-  return "Value " + obj[key];
+  console.log("Accessor decorator!");
+  console.log(target, name, descriptor);
 }
 
-console.log(extractAndConvert({ name: "Nandu" }, "name"));
-
-class DataStorage<T extends string | number | boolean> {
-  private data: T[] = [];
-
-  addItem(item: T) {
-    this.data.push(item);
-  }
-
-  removeItem(item: T) {
-    if (this.data.indexOf(item) == -1) return;
-
-    this.data.splice(this.data.indexOf(item), 1);
-  }
-
-  getItems() {
-    return [...this.data];
-  }
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
+  console.log(target, name, descriptor);
 }
 
-const textStorage = new DataStorage<string>();
-textStorage.addItem("Max");
-textStorage.addItem("Manu");
-textStorage.removeItem("Manu");
-console.log(textStorage.getItems());
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter decorator!");
+  console.log(target, name, position);
+}
 
-// const objStorage = new DataStorage<object>();
-// objStorage.addItem({name:'Max'});
-// objStorage.addItem({name:'Manu'});
-// objStorage.removeItem({name:'Max'});
-// console.log(objStorage.getItems());
-
-
-//Generic Utitlity Types - Partial and ReadOnly
-interface CourseGoal {
+class Product {
+  @Log
   title: string;
-  description: string;
-  completeUntil: Date;
+  private _price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) this._price = val;
+    else new Error("Value cant be negative");
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
 }
 
-function createCourseGoal(
-  title: string,
-  description: string,
-  date: Date
-): CourseGoal {
-  let courseGoal: Partial<CourseGoal> = {};
-  courseGoal.title = title;
-  courseGoal.description = description;
-  courseGoal.completeUntil = date;
-  return courseGoal as CourseGoal;
+//Auto Bind Method Decorator example
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      return originalMethod.bind(this);
+    },
+  };
+  return adjDescriptor;
 }
 
-//works for objects too
-const names : Readonly<string[]> = ["Nanda","Kishore"];
-//names.push("dummy");    // push not allowed
+class Printer {
+  message = "This Works!!";
+  @AutoBind
+  showMessage() {
+    console.log(this.message);
+  }
+}
 
-//Use Generic types when you have to lockin to a particular type for an instance , best use case is in classes
-//use union types when flexibility is needed when passing paramaters in a function call
+const p = new Printer();
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
+
+
+//Validation using Decorators
+
+interface ValidatorConfig{
+  [property : string] : {
+    [validatableProp : string] : string[]; // ['required','positive']
+  }
+}
+
+const registeredValidators : ValidatorConfig = {};
+
+function RequiredInput(target : any,propName : string){
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName] : [...registeredValidators[target.constructor.name][propName],'required']
+  }
+}
+
+function PositiveNumber(target : any,propName : string){
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName] : [...registeredValidators[target.constructor.name][propName],'positive']
+  }
+}
+
+function validate(obj : any){
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if(!objValidatorConfig){
+    return true;
+  }
+
+  let isValid = true;
+  for(const prop in objValidatorConfig){
+    for(const validator of objValidatorConfig[prop]){
+      switch(validator){
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+
+  @RequiredInput
+  title: string;
+
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit',event => {
+  event.preventDefault();
+  const titleEl = document.getElementById('title') as HTMLInputElement;
+  const priceEl = document.getElementById('price') as HTMLInputElement;
+  const createdCourse = new Course(titleEl.value,+priceEl.value);
+  if(!validate(createdCourse)){
+    alert('Invalid input.Please try again');
+    return;
+  }
+  console.log(createdCourse);
+})
